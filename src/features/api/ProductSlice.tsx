03f844379ee5e5b5311a3d3aef2261d4b815bce0
macadadi <macadadi36@gmail.com>
2021-09-28 14:54:40 +0300

    created product slice

---
+++ src/features/api/ProductSlice.tsx
@@ -0,0 +1,85 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState, AppThunk } from '../../app/store';
+
+
+export interface productState {
+  name?: string;
+  brand?:string;
+  price:number ;
+  category?:string;
+  description?:string;
+  status: 'idle' | 'loading' | 'failed' |'fullfiled';
+}
+
+const initialState: productState = {
+  name :'',
+  brand:'',
+  price :0,
+  category:'',
+  description:'',
+  status: 'idle',
+};
+
+// The function below is called a thunk and allows us to perform async logic. It
+// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
+// will call the thunk with the `dispatch` function as the first argument. Async
+// code can then be executed and other actions can be dispatched. Thunks are
+// typically used to make async requests.
+export const fetchdata = createAsyncThunk(
+  'counter/fetchCount',
+  async ( url : string) => {
+    const response = await fetch(url)
+        .then(data=>data.json())
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+
+export const productSlice = createSlice({
+  name: 'product',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    increment: (state) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.price += 1;
+    },
+    decrement: (state) => {
+      state.price -= 1;
+    },
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.price += action.payload;
+    },
+  },
+ 
+
+  extraReducers: (result) => {
+    result
+      .addCase(fetchdata.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchdata.fulfilled, (state, action) => {
+        state.status = 'fullfiled';
+        state.price += action.payload;
+      })
+      .addCase(fetchdata.rejected, (state) => {
+        state.status = 'failed';
+       
+      })
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = productSlice.actions;
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
+export const selectCount = (state: RootState) => state.counter.value;
+
+
+
+export default productSlice.reducer;
